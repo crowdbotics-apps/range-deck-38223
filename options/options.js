076de207d878a616ba95291e7593ec78507d839a
Crowdbotics <team@crowdbotics.com>
2023-01-08 02:48:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "range-deck-38223",
+  url: "https://range-deck-38223.botics.co",
+  api: "https://range-deck-38223.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
